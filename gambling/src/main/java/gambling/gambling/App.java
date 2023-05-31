7b9fbc0e5faca9f7d4c1557589934e0a0d6f445a
carlos <carlos@Carlos>
2023-05-31 18:12:44 +0200

    A VER

--- gambling/src/main/java/gambling/gambling/App.java
+++ gambling/src/main/java/gambling/gambling/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("s");
+        System.out.println("pruebas");
+        System.out.println("100%");
     }
 }
